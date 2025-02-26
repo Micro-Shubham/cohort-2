77254c8ab4106e2d18a211243149e8deb4f98ee9
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-02-27 03:25:32 +0530

    zod

---
+++ week-5.2/backend/index.js
@@ -0,0 +1,16 @@
+// express
+const express =  require('express');
+const app = express();
+app.use(express.json());
+//post function 
+app.post("/todo", function(req, res) {
+
+})
+//get function 
+app.get("/todos", function(req, res) {
+
+})
+//put function 
+app.put("/completed", function(req, res) {
+
+})
