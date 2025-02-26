38ac0961c60da015778740217246ac20bc831e08
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-02-27 03:35:12 +0530

    routes

--- week-5.2/backend/index.js
+++ week-5.2/backend/index.js
@@ -1,9 +1,19 @@
 // express
 const express =  require('express');
+const { createTodo, updateTodo } = require('./type');
 const app = express();
 app.use(express.json());
 //post function 
 app.post("/todo", function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createTodo);
+    if(parsedPayload.success) {
+        res.status(411).json( {
+            msg:"You sent the wrong input"
+        })
+        return;
+    }
+    // put in mongodb
 
 })
 //get function 
@@ -12,5 +22,13 @@ app.get("/todos", function(req, res) {
 })
 //put function 
 app.put("/completed", function(req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(parsedPayload.success) {
+        res.status(411).json({
+            msg:"You have send wrong input"
+        })
+        return ;
+    }
 
 })
