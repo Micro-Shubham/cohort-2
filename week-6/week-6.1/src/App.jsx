798dbb21c70e1965530fca600ef7b334944a1b6e
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-06 17:54:03 +0530

    Refactor App component to update title dynamically and improve button functionality

--- week-6/week-6.1/src/App.jsx
+++ week-6/week-6.1/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
 
 function App() {
-
+  // usestate
+  const [title, setTitle] = useState("My name is hakirat")
+ function upDateTitle() {
+  setTitle("my name is "+Math.random())
+ }
   return  (
     // if you don't want to put div as parent you can simply
     // react fragmentation  <React.Fragment> </React.Fragment>
     // or just simply <> </>
     <>
-      <Header title="Harkirat1"></Header>
-      <Header title="Harkirat2"></Header>
+    <button onClick={upDateTitle}>Click here to change the first header</button>
+      <Header title={title}></Header>
+      <Header title="My name is Raman"></Header>
     </>
   )
   
