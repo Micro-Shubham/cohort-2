0b5c5b20e9877c6f9a38fda494119f2cd3a159b7
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-06 17:29:55 +0530

    Refactor App component to use React Fragment instead of a div

--- week-6/week-6.1/src/App.jsx
+++ week-6/week-6.1/src/App.jsx
@@ -2,10 +2,13 @@
 function App() {
 
   return  (
-    <div>
+    // if you don't want to put div as parent you can simply
+    // react fragmentation  <React.Fragment> </React.Fragment>
+    // or just simply <> </>
+    <>
       <Header title="Harkirat1"></Header>
       <Header title="Harkirat2"></Header>
-    </div>
+    </>
   )
   
      
