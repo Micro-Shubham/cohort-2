3a237b14ccb5218d736ffedc0ddc91a7da5d53bb
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 00:11:28 +0530

    Refactor App component to remove unused title state and button, maintaining HeaderWithButton functionality

--- week-6/week-6.1/src/App.jsx
+++ week-6/week-6.1/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from "react"
 
 function App() {
   // usestate
-  const [title, setTitle] = useState("My name is hakirat")
- function upDateTitle() {
-  setTitle("My name is "+Math.random())
- }
+  // const [title, setTitle] = useState("My name is hakirat")
+//  function upDateTitle() {
+  // setTitle("My name is "+Math.random())
+//  }
   return  (
     // if you don't want to put div as parent you can simply
     // react fragmentation  <React.Fragment> </React.Fragment>
     // or just simply <> </>
     <div>
-    <button onClick={upDateTitle}>Click here to change the first header</button>
-      <Header title={title}></Header>
+    {/* <button onClick={upDateTitle}>Click here to change the first header</button> */}
+      {/* <Header title={title}></Header> */}
+      <HeaderWithButton></HeaderWithButton>
       <Header title="My name is Harkirat Singh2"></Header>
       <Header title="My name is Harkirat Singh3"></Header>
       <Header title="My name is Harkirat Singh4"></Header>
@@ -22,6 +23,24 @@ function App() {
   
      
   
+}
+// this help to avoid re rendering whole parents div 
+// to avoid we push it to the component
+// when click on button it re render only that component not whole div
+// goal is to make our application more optimal 
+function HeaderWithButton() {
+  const [title, setTitle] = useState("My name is hakirat")
+   function upDateTitle() {
+  setTitle("My name is "+Math.random())
+ }
+
+  return (
+    <>
+    <button onClick={upDateTitle}>Click here to change the first header</button>
+      <Header title={title}></Header>
+    </>
+  )
+
 }
 function Header({title}) {
   return <div>
