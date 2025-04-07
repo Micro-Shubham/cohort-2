f73263150860b47cb68ea09b475e1da8de2878c9
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 02:49:08 +0530

    Refactor App and CardWrapper components to use children prop for improved flexibility and clarity

--- week-6/wrapper/src/App.jsx
+++ week-6/wrapper/src/App.jsx
@@ -3,24 +3,35 @@ import { useState } from "react";
 
 function App() {
   return (
+    // <div>
+    //   <CardWrapper innerComponent={<TextComponent />} />
+    //   <CardWrapper innerComponent={<TextComponent2 />} />
+    // </div>
+    // there is something called real wrapper
+
     <div>
-      <CardWrapper innerComponent={<TextComponent />} />
-      <CardWrapper innerComponent={<TextComponent2 />} />
+      <CardWrapper>
+        Hi there
+      </CardWrapper>
+      <CardWrapper>
+        Hello there
+      </CardWrapper>
     </div>
-  );
+  )
+
 }
 
 // wrapper accept a component as input
-function CardWrapper({ innerComponent }) {
+function CardWrapper({ children }) {
   // create  a div  which has a border
   // and inside  the div  , render  the prop
-  return <div style={{ border: "2px solid black",padding:"20px",margin:"5px" }}>{innerComponent}</div>;
+  return <div style={{ border: "2px solid black",padding:"20px",margin:"5px" }}>{children}</div>;
 }
-function TextComponent() {
-  return <div>hi there</div>;
-}
-function TextComponent2() {
-  return <div>Hi there 2</div>
-}
+// function TextComponent() {
+//   return <div>hi there</div>;
+// }
+// function TextComponent2() {
+//   return <div>Hi there 2</div>
+// }
 
 export default App;
