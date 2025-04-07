10e4f3e8cd304ad2ee25d86fc4a65d091b43daad
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 01:40:55 +0530

    Refactor App component for improved readability and consistency in code style

--- week-6/keys/src/App.jsx
+++ week-6/keys/src/App.jsx
@@ -1,37 +1,69 @@
-
-
+import React from "react";
+import { useState } from "react";
 function App() {
   // state that has already three todo in it
-  const [todos , setTodos] = usestate([
+  const [todos, setTodos] = useState([
     {
-      id:1,
-      title:"go to gym",
-      description:"go to gym today"
+      id: 1,
+      title: "go to gym",
+      description: "go to gym today",
     },
     {
-      id:1,
-      title:"go to market",
-      description:"go to market today"
-    }, 
+      id: 1,
+      title: "go to market",
+      description: "go to market today",
+    },
     {
-      id:1,
-      title:"go to school",
-      description:"go to school today"
-    },,
-  ])
+      id: 1,
+      title: "go to school",
+      description: "go to school today",
+    },
+    ,
+  ]);
+  // one way to add new todo
 
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: 4,
+        title: Math.random(),
+        description: Math.random(),
+      },
+    ]);
+    // another way to add new todo
+    // const newTodos = [];
+    // for (let i = 0; i < todos.length; i++) {
+    //   newTodos.push(todos[i]);
+    // }
+    // // newtodos == todos
+    // newTodos.push({
+    //   id: 4,
+    //   title: Math.random(),
+    //   description: Math.random(),
+    // });
+    // setTodos(newTodos);
+  }
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <button onClick={addTodo}>Add a Todo</button>
+      {/* there  is two ways to do it  */}
+     {todos.map(todo => <Todo title={todo?.title} description={todo?.description}/>)}
+      {/* or  */}
+      {/* {todos.map(function (todo) {
+        return <Todo title={todo.title} description={todo.description} />
+      })} */}
+    </div>
+  );
 }
 // title and description
-function Todo (title , description) {
-  return <div>
-    <h1>{title}</h1>
-    <h5>{description}</h5>
-  </div>
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 }
 
-export default App
+export default App;
