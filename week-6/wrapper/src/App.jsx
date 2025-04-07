50e5556f3d81c49e77ad2b213f67b1359e1bb7d1
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 02:42:46 +0530

    Refactor App component structure and improve CardWrapper implementation for clarity

--- week-6/wrapper/src/App.jsx
+++ week-6/wrapper/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 function App() {
-  
-
   return (
-    <>
-
-    </>
-  )
+    <div>
+      <CardWrapper innerComponent={<TextComponent />} />
+      <CardWrapper innerComponent={<TextComponent2 />} />
+    </div>
+  );
 }
 
-export default App
+// wrapper accept a component as input
+function CardWrapper({ innerComponent }) {
+  // create  a div  which has a border
+  // and inside  the div  , render  the prop
+  return <div style={{ border: "2px solid black",padding:"20px",margin:"5px" }}>{innerComponent}</div>;
+}
+function TextComponent() {
+  return <div>hi there</div>;
+}
+function TextComponent2() {
+  return <div>Hi there 2</div>
+}
+
+export default App;
