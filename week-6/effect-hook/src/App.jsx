e1d1f4cbbe36863aa3ac5e588a9c073ec96e2519
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 23:29:41 +0530

    Refactor App and Todo components to enhance structure and dynamic data fetching

--- week-6/effect-hook/src/App.jsx
+++ week-6/effect-hook/src/App.jsx
@@ -1,34 +1,68 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 function App() {
+  // usetstat
+  const[selectedId, setSelectedId] = useState(1);
 
-
-
-  return <>
-   <Todo id={1} />
-  </>;
+  return (
+    <>
+      <button
+        onClick={function () {
+          setSelectedId(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={function () {
+         setSelectedId(3);
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={function () {
+         setSelectedId(4);
+        }}
+      >
+        4
+      </button>
+      <button
+        onClick={function () {
+          setSelectedId(5);
+        }}
+      >
+        5
+      </button>
+      <Todo id={selectedId} />
+    </>
+  );
 }
 // component
-function Todo({id}) {
-  const [todo, setTodo] = useState({})
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
   // implement useEffect here
   useEffect(() => {
-   axios.get("https://dummyjson.com/todos/"+id)
-    .then(response => {
-      setTodo(response.data)
-    })
-  },[])
+    axios.get(`https://dummyjson.com/todos/${id}`).then((response) => {
+      setTodo(response.data);
+    });
+  }, [id]);
 
-  return <div>
-    <h1>
-      {todo.todo}
-    </h1>
-    <h1>
-      {todo.userId}
-    </h1>
-  </div>
-
-
+  return (
+    
+    <div>
+      Id:{id}
+      <h1>{todo.todo}</h1>
+      <h1>{todo.userId}</h1>
+    </div>
+  );
 }
 
 export default App;
