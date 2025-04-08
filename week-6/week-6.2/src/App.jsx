ec7dc660fce367cc053e790f7254291311f34a18
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-08 20:58:11 +0530

    Refactor Todo component to improve prop usage and enhance code clarity

--- week-6/week-6.2/src/App.jsx
+++ week-6/week-6.2/src/App.jsx
@@ -1,31 +1,30 @@
-import React from "react"
-import { useEffect, useState } from 'react'
-import axios from "axios"
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-const [todos, setTodos] = useState([]); 
-useEffect(() => {
- axios.get("https://dummyjson.com/todos")
-  .then(function(response) {
-     setTodos(response.data.todos)
-  })
-},[])
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios.get("https://dummyjson.com/todos").then(function (response) {
+      setTodos(response.data.todos);
+    });
+  }, []);
 
   return (
     <>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
-     
+      {todos.map(todo => <Todo key={todo.id} title={todo.todo} completed={todo.completed} />)}
     </>
-  )
+  );
 }
-
 
 // creating a component
-function Todo({title, description} ) {
-  return <div>
-   <h1>{title} </h1>
-   <h4>{description}</h4>
-  </div>
+function Todo({ title, completed }) {
+  return (
+    <div>
+      <h1>{title} </h1>
+      <h4>Status: {completed ? "✅ Completed" : "❌ Not Completed"}</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
