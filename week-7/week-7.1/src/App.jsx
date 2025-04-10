d273f7b5f9bb07ae7871c3686550b2baf1370651
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-10 23:22:30 +0530

    Add routing with Dashboard and Landing components

--- week-7/week-7.1/src/App.jsx
+++ week-7/week-7.1/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Dashboard } from "./Components/Dashboard"
+import { Landing } from "./Components/Landing"
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div>
+      <div style={{background: "black" , color:"white"}}>
+      Hi there from topbar 
+      </div>
+   <BrowserRouter>
+   <Routes>
+      <Route  path="/Dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Landing />} /> 
+   </Routes>
+
+   </BrowserRouter> 
+   </div>
   )
 }
 
