c63f049c5b0ecb270d0d5b4ece4134502509dc6d
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-14 21:30:06 +0530

    used atomfamily for state management

--- week-7/atom-family/src/App.jsx
+++ week-7/atom-family/src/App.jsx
@@ -1,12 +1,27 @@
-
+import {RecoilRoot, useRecoilValue} from "recoil"
+import { todosAtomFamily } from "./atom"
 function App() {
  
 
   return (
     <>
-      
+      <RecoilRoot>
+        <Todo id={1}/>
+        <br />
+        <Todo id={2}/>
+
+      </RecoilRoot>
     </>
   )
+}
+// component
+function Todo({id}) {
+  const currentTodo = useRecoilValue(todosAtomFamily(id))
+  return <>
+  {currentTodo.title}
+  <br />
+  {currentTodo.description}
+  </>
 }
 
 export default App
