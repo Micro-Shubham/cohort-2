4476e907f4a81bcb18322f2079f3b8b7c17042ec
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-15 23:05:04 +0530

    refactor layout: update flex container to use justify-between for better spacing

--- week-8/tailwind-prac/src/App.jsx
+++ week-8/tailwind-prac/src/App.jsx
@@ -6,9 +6,10 @@ function App() {
 
   return (
     <>
-     <div className='flex'>
+     <div className='flex justify-between'>
        <div style={{backgroundColor:"red"}}>hi there</div>
-       <div style={{backgroundColor:"blue"}}>hi there</div>
+    <div style={{backgroundColor:"blue"}}>hi there</div>
+     <div className='bg-emerald-300'>Hi there bro</div>
      </div>
     </>
   )
