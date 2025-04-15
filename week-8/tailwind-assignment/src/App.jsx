1844ae4bef79be28d74b5b99691d8b4adc0fdbcf
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-16 02:24:05 +0530

    tailwind-assignment

---
+++ week-8/tailwind-assignment/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="bg-blue-400 "> Hi there</div>
+      <div className="bg-red-400 "> Hi there</div>
+      <div className="bg-violet-400 "> Hi there</div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
