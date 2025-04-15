12cee86d78f8eba242a3b62ef0299281e65e652e
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-16 01:41:30 +0530

    basic grid

---
+++ week-8/tailwind-grid/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* // understanding grid */}
+      <div className="grid grid-cols-3">
+        <div className="bg-blue-900"> Hi </div>
+        <div className="bg-pink-300"> Hi </div>
+        <div className="bg-red-900"> Hi </div>
+        <div className="bg-green-900"> Hi </div>
+        <div className="bg-blue-900"> Hi </div>
+        <div className="bg-yellow-900"> Hi </div>
+        <div className="bg-yellow-900"> Hi </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
