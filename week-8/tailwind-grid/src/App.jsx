c16625aa82d17059fd74d0aaa985769efbb76387
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-16 01:51:11 +0530

    col-span and grid-col-12

--- week-8/tailwind-grid/src/App.jsx
+++ week-8/tailwind-grid/src/App.jsx
@@ -6,14 +6,10 @@ function App() {
   return (
     <>
       {/* // understanding grid */}
-      <div className="grid grid-cols-3">
-        <div className="bg-blue-900"> Hi </div>
-        <div className="bg-pink-300"> Hi </div>
-        <div className="bg-red-900"> Hi </div>
-        <div className="bg-green-900"> Hi </div>
-        <div className="bg-blue-900"> Hi </div>
-        <div className="bg-yellow-900"> Hi </div>
-        <div className="bg-yellow-900"> Hi </div>
+      <div className="grid grid-cols-12">
+        <div className="bg-blue-900 col-span-6  "> Hi </div>
+        <div className="bg-pink-300 col-span-4"> Hi </div>
+        <div className="bg-red-900 col-span-2"> Hi </div>
 
       </div>
 
