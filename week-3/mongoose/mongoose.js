0a228a7b3d09d8239a826cd338e0c1b2c74e3137
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-20 01:19:10 +0530

    mongoose and chekcing users

---
+++ week-3/mongoose/mongoose.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://user:Userid@cluster0.7pbjx.mongodb.net/")
+
+const User = mongoose.model('Users', {name: String, email:String, password:String})
+
+app.post("/signup",async function(req, res) {
+    const username  = req.body.username;
+    const password  = req.body.password;
+    const name      = req.body.name;
+
+    const existingUser = await User.findOne({email:username})
+
+    if(existingUser) {
+        return res.status(400).send("Username already exists");
+    }
+
+
+    const user = new User({
+        name: name,
+        email: username,
+        password: password
+    })
+
+    user.save();
+    res.json({
+        "msg":" user created succussfully"
+    })
+    
+})
