8a379e5515ad33d706eda28e9b5ca897c13dca1c
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-06 09:03:45 +0530

    class based component

--- week-9/custom-hook/src/App.jsx
+++ week-9/custom-hook/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { useState } from "react"
 
 function App() {
@@ -10,15 +11,36 @@ function App() {
   )
 }
 
-function MyComponent() {
-  const [count, setCount] = useState(0);
-  const Increament=() => {
-     setCount(count+1);
-  }
-  return <div>
-    <h1>{count}</h1>
-    <button onClick={Increament}>increment</button>
-  </div>
-}
+// function MyComponent() {
+//   const [count, setCount] = useState(0);
+//   const Increament=() => {
+//      setCount(count+1);
+//   }
+//   return <div>
+//     <h1>{count}</h1>
+//     <button onClick={Increament}>increment</button>
+//   </div>
+// }
 
+
+// class based component
+class MyComponent extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {count:0}
+  }
+
+  incrementCount =() =>{
+    this.setState({count:this.state.count+1});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    )
+  }
+}
 export default App
