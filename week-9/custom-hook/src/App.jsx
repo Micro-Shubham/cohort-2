f577f21f8c92ca40321a3094e005ecdd0e0a7fae
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-06 08:55:46 +0530

    understanding diff bwt class based component and function based component

--- week-9/custom-hook/src/App.jsx
+++ week-9/custom-hook/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 
 function App() {
   
   return (
     <>
-      
+      <MyComponent />
   
     </>
   )
+}
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const Increament=() => {
+     setCount(count+1);
+  }
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={Increament}>increment</button>
+  </div>
 }
 
 export default App
