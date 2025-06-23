580774fa020a4445e54d21813c96db1fd7e1e694
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-06-24 02:33:52 +0530

    add sumation function in TypeScript for calculating the sum of two numbers

---
+++ week-9/typscript/sum.js
@@ -0,0 +1,5 @@
+"use strict";
+function sumation(a, b) {
+    console.log("sum : " + (a + b));
+}
+sumation(4, 5);
---
+++ week-9/typscript/sum.ts
@@ -0,0 +1,5 @@
+function sumation(a:number, b:number) {
+    console.log("sum : " + (a+b));
+}
+
+sumation(4,5);
